aecc677712884f1d85379b61b89b8aadb872cf90
Marcos Lucas <mac@192.168.100.11>
2024-09-24 11:13:11 -0300

    feitos

---
+++ hello-word/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello world react</h1>
+     <h2>meu deus</h2>
+    </div>
+    
+  );
+}
+
+export default App;
+
